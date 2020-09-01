e505e00bfef0ecb5c2ca8b1189210b0e2adb0783
SergeyZheleznov <jeleznov_sv@mail.ru>
2020-09-01 12:30:58 +0300

    Правка 2 от 01.09.2020 задание 9 ветка module9-task1

--- gulpfile.js
+++ gulpfile.js
@@ -19,22 +19,22 @@ const htmlmin = require("gulp-htmlmin");
 
 //Styles
 
-const stylesmin = () => {
-  return gulp.src("source/less/style.less")
-    .pipe(plumber())
-    .pipe(sourcemap.init())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
-    .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("build/css"))
-    .pipe(sync.stream());
-}
+//const stylesmin = () => {
+//  return gulp.src("source/less/style.less")
+//    .pipe(plumber())
+//    .pipe(sourcemap.init())
+//    .pipe(less())
+//    .pipe(postcss([
+//      autoprefixer()
+//    ]))
+//    .pipe(csso())
+//    .pipe(rename("style.min.css"))
+//    .pipe(sourcemap.write("."))
+//    .pipe(gulp.dest("build/css"))
+//    .pipe(sync.stream());
+//}
 
-exports.stylesmin = stylesmin;
+//exports.stylesmin = stylesmin;
 
 const styles = () => {
   return gulp.src("source/less/style.less")
@@ -45,6 +45,10 @@ const styles = () => {
       autoprefixer()
     ]))
     .pipe(rename("style.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(sync.stream());
@@ -86,18 +90,20 @@ const html = () => {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(gulp.dest("build"))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("build"));
 }
 
 exports.html = html;
 
-const minify = () => {
-  return gulp.src("source/*.html")
-  .pipe(htmlmin({ collapseWhitespace: true }))
-  .pipe(gulp.dest("build"));
-}
+//const minify = () => {
+//  return gulp.src("source/*.html")
+//  .pipe(htmlmin({ collapseWhitespace: true }))
+//  .pipe(gulp.dest("build"));
+//}
 
-exports.minify = minify;
+//exports.minify = minify;
 
 const clean = () => {
   return delt("build");
@@ -136,14 +142,14 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
-  gulp.watch("source/*.html", gulp.series(html, minify));
+  gulp.watch("source/*.html", gulp.series(html));
 }
 
 exports.default = gulp.series (
-  stylesmin, styles, server, watcher
+  styles, server, watcher
 );
 
-const build = gulp.series(clean, copy, stylesmin, styles, sprite, html, minify);
+const build = gulp.series(clean, copy, styles, sprite, html);
 exports.build = build;
 
 const start = gulp.series(build, server);
